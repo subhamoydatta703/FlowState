fa4f1bcf68bf95dc3b2a0b993c28d08dda61456b
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-01-14 10:38:28 +0530

    "Adding web analytics"

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import './App.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { ThemeProvider } from './context/ThemeContext'
+import { Analytics } from '@vercel/analytics/react'
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -56,6 +57,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <ThemeProvider>
           <App />
+          <Analytics />
         </ThemeProvider>
       </ClerkProvider>
     </ErrorBoundary>
